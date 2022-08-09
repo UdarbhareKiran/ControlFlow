8b2b96b6dc14a0bb724d772f03e3579d073fe81b
UdarbhareKiran <kpujari42@gmail.com>
2022-08-09 12:07:45 +0530

    Add Control Flow

---
+++ src/ControlFlow/IfelseDemo.java
@@ -0,0 +1,33 @@
+package ControlFlow;
+
+public class IfelseDemo {
+
+	public static void main(String[] args) {
+		
+		//case1-if else
+		int age=20;
+		if(age>=20)
+		{ System.out.println("Eligble for marrige");
+		}else
+		{System.out.println("not eligble marrige");
+			
+		}
+	
+
+        //case-2
+
+if(true )
+{ System.out.println("True body");
+}else
+{System.out.println("false body");
+//}}
+}}}
+
+//case-3
+//if(0 )
+//{ System.out.println("True body");
+//}else
+//{System.out.println("false body");
+//
+//}}
+//}
