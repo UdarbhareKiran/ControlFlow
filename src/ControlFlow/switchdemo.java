27ccc6285937ffcc99d9f469d20e3a6d9d382602
UdarbhareKiran <kpujari42@gmail.com>
2022-08-09 12:44:46 +0530

    switch case added

---
+++ src/ControlFlow/switchdemo.java
@@ -0,0 +1,20 @@
+package ControlFlow;
+
+public class switchdemo {
+	
+	public static void main(String[] args) {
+		
+		//case1
+		int a=10;
+		switch(a) {
+		
+		case 25 :System.out.println("Kiran");
+		break;
+		case 120:System.out.println("Swati");
+		break;
+		default:System.err.println("ram");
+		}
+		
+	}
+
+}
